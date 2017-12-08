49b4bc1814790b97f3fe9ea3f8c19adbb709bfcd
J3Viton <j3viton@gmail.com>
2017-12-08 23:43:51 +0100

    'empezando'

---
+++ R3D3.cpp
@@ -0,0 +1,70 @@
+/*
+  R3D3
+  Libreria para controlar el robot de Kiddybot R3D3
+    Created by J3Viton [Kiddybot], November, 2017.
+
+
+    Hardware:
+        DC motor Driver:
+        L911O h bridge  http://www.gie.com.my/UploadFiles/robotics/drivers/motor_waveform.jpg
+*/
+
+#include "Arduino.h"
+#include "R3D3.h"
+
+R3D3::R3D3(int pin)
+{
+  pinMode(11, OUTPUT);
+  _pin = pin;
+
+    pinMode(LED_RED, OUTPUT);
+    pinMode(LED_GREEN, OUTPUT);
+}
+
+
+void R3D3::test_bridgeH(int motor, int speed, int rotation){
+    pinMode(MotorA_IA, OUTPUT);
+    pinMode(MotorA_IB, OUTPUT);
+
+    // Turn Righ
+    digitalWrite(MotorA_IA, HIGH);
+    digitalWrite(MotorA_IB, LOW);
+    digitalWrite(LED_GREEN, HIGH);
+    delay(100);
+    // Stop
+    digitalWrite(MotorA_IA, LOW);
+    digitalWrite(MotorA_IB, LOW);
+    digitalWrite(LED_GREEN, LOW);
+    delay(1000);
+    // Turn Left
+    digitalWrite(MotorA_IA, LOW);
+    digitalWrite(MotorA_IB, HIGH);
+    digitalWrite(LED_RED, HIGH);
+    delay(100);
+    // Stop
+    digitalWrite(MotorA_IA, LOW);
+    digitalWrite(MotorA_IB, LOW);
+    digitalWrite(LED_RED, LOW);
+    delay(1000);
+
+
+  }//test_bridgeH  FIN
+
+
+
+void R3D3::dot()
+{
+  digitalWrite(_pin, HIGH);
+  delay(250);
+  digitalWrite(_pin, HIGH);
+  delay(250);
+}
+
+void R3D3::dash()
+{
+  digitalWrite(_pin, HIGH);
+  delay(1000);
+  digitalWrite(_pin, LOW);
+
+ delay(250);
+}
---
+++ R3D3.h
@@ -0,0 +1,31 @@
+/*
+  R3D3  Library.
+  Created by J3Viton [Kiddybot], November, 2017.
+*/
+#ifndef R3D3_h
+#define R3D3_h
+
+#include "Arduino.h"
+
+#define LED_RED   11
+#define LED_GREEN 10
+
+#define MotorB_IB 6
+#define MotorB_IA 7
+#define MotorA_IB 4
+#define MotorA_IA 5
+
+class R3D3
+{
+  public:
+    R3D3(int pin);
+    void test_bridgeH(int motor, int speed, int rotation);
+    void dot();
+    void dash();
+  private:
+    int _pin;
+  //  int MotorA_IB=6;
+  //  int MotorA_IA=7;
+};
+
+#endif
